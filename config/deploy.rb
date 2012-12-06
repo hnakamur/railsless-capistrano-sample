c904f1eed9b6294bc098855bf46a48611b321fd7
Hiroaki Nakamura <hnakamur@gmail.com>
2012-12-06 17:20:22 +0900

    Use local timestamp for deploy folders.

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,9 @@ set :scm, :none
 set :deploy_via, :copy
 set :copy_compression, :bz2
 set :deploy_to, '/tmp/foo'
+set(:release_name) {
+  set :deploy_timestamped, true; Time.now.strftime("%Y%m%d%H%M%S")
+}
 set :normalize_asset_timestamps, false
 
 role :web, 'cent6'
