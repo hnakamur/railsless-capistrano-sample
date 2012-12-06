b8c104768c5c893bdc688e333126d47f1c58c6f8
Hiroaki Nakamura <hnakamur@gmail.com>
2012-12-06 17:14:42 +0900

    use bzip2.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,5 +2,6 @@ set :application, "sample1"
 set :repository,  "."
 set :scm, :none
 set :deploy_via, :copy
+set :copy_compression, :bz2
 set :deploy_to, '/tmp/foo'
 set :normalize_asset_timestamps, false
